63b519acdd093312f68801069b5a3184281494cd
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-05-26 17:47:36 +0200

    Replace outdated file with final version of today's work

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,4 +1,5 @@
 import random
+from random import choice
 
 rock = '''
     _______
@@ -27,91 +28,58 @@ scissors = '''
 ---.__(___)
 '''
 
+hand_drawing = [rock, paper, scissors]
+
 # Rules of the logic:
 # Rock wins against scissors
 # Paper wins against rock
 # Scissors wins against paper
 
-# first request for the input: the user enter "rock", "scissors" or "paper"
-# and take the result in each variable
-# then, the computer enter a random choice and take the result in a variable
-# aply the logic
-
-# decide if you win or you lost
-
-# recoger la elección de cada participante:
+# recoger la elección del user:
 my_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors?\n"))
+# pintar la elección del user:
+#print(hand_drawing[my_choice])
+print(my_choice)
+
+#generar la elección aleatoria de computer:
 computer_choice = random.randint(0, 2)
+# pintar la elección de computer:
+# print(f"Computer Choose: {hand_drawing[computer_choice]}")
 print(f"Computer Choose: {computer_choice}")
 
 #Lógica del juego:
+# si yo saco piedra y computer saca piedra, empate
+# si yo saco piedra y computer saca papel, yo pierdo
 # si yo saco piedra y computer saca tijeras, yo gano
+
 # si yo saco paper y computer saca rock, yo gano
+# si yo saco paper y computer saca tijeras, yo pierdo
+# si yo saco paper y computer saca papel, empate
+
 # si yo saco tijeras y computer saca paper, yo gano
+# si yo saco tijeras y computer saca rock, yo pierdo
+# si yo saco tijeras y computer saca tijeras, empate
 
-# si son iguales, empatamos
-
-# # si yo saco piedra y computer saca papel, yo pierdo
-# # si yo saco paper y computer saca tijeras, yo pierdo
-# # si yo saco tijeras y computer saca rock, yo pierdo
-
-if my_choice == 0:
-    print(rock)
-    if computer_choice == 0:
-        print(rock)
-        print("This is a tie")
-    elif computer_choice == 1:
-        print(paper)
-        print("You win")
-    elif computer_choice == 2:
-        print(scissors)
-        print("You loose")
-    else:
-        print("You are type a invalid choice")
-else:
+if my_choice > 2 or my_choice < 0:
     print("You are type an invalid number. You lose!")
-
-if my_choice == 1:
-    print(paper)
-    if computer_choice == 0:
-        print(rock)
-        print("You loose")
-    elif computer_choice == 1:
-        print(paper)
-        print("This is a tie")
-    elif computer_choice == 2:
-        print(scissors)
-        print("You win")
-    else:
-        print("You are type a invalid choice")
-else:
-    print("You are type an invalid number. You lose!")
-
-if my_choice == 2:
-    print(scissors)
-    if computer_choice == 0:
-        print(rock)
-        print("You loose")
-    elif computer_choice == 1:
-        print(paper)
-        print("You win")
-    elif computer_choice == 2:
-        print(scissors)
-        print("This is a tie")
-    else:
-        print("You are type a invalid choice")
-else:
-    print("You are type an invalid number. You lose!")
-
-'''
-rock_paper_scissors = [0, 1, 2]
-
-my_choice[0] = "Rock"
-my_choice[1] = "Paper"
-my_choice[2] = "Scissors"
-
-print(rock_paper_scissors)
-'''
-
-
-
+# rock against ...
+elif my_choice == 0 and computer_choice == 0:
+    print("This is a tie")
+elif my_choice == 0 and computer_choice == 1:
+    print("You loose")
+elif my_choice == 0 and  computer_choice == 2:
+    print("You win")
+# paper against...
+elif my_choice == 1 and computer_choice == 0:
+    print("You win")
+elif my_choice == 1 and computer_choice == 1:
+    print("This is a tie")
+elif my_choice == 1 and computer_choice == 2:
+    print("You loose")
+# scissors against...
+elif my_choice == 2 and computer_choice == 0:
+    print("You loose")
+elif my_choice == 2 and computer_choice == 1:
+    print("You win")
+elif my_choice == 2 and computer_choice == 2:
+    print("This is a tie")
