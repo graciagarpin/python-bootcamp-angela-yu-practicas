c7726e23c5caaeadced03b31fcdf3cec1f4428ae
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-05-27 17:54:52 +0200

    fix the statement to control the entry of an invalid number

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -30,40 +30,22 @@ scissors = '''
 
 hand_drawing = [rock, paper, scissors]
 
-# Rules of the logic:
-# Rock wins against scissors
-# Paper wins against rock
-# Scissors wins against paper
-
-# recoger la elección del user:
+# Recoger la elección del user:
 my_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors?\n"))
-# pintar la elección del user:
-#print(hand_drawing[my_choice])
-print(my_choice)
-
-#generar la elección aleatoria de computer:
-computer_choice = random.randint(0, 2)
-# pintar la elección de computer:
-# print(f"Computer Choose: {hand_drawing[computer_choice]}")
-print(f"Computer Choose: {computer_choice}")
-
-#Lógica del juego:
-# si yo saco piedra y computer saca piedra, empate
-# si yo saco piedra y computer saca papel, yo pierdo
-# si yo saco piedra y computer saca tijeras, yo gano
-
-# si yo saco paper y computer saca rock, yo gano
-# si yo saco paper y computer saca tijeras, yo pierdo
-# si yo saco paper y computer saca papel, empate
-
-# si yo saco tijeras y computer saca paper, yo gano
-# si yo saco tijeras y computer saca rock, yo pierdo
-# si yo saco tijeras y computer saca tijeras, empate
-
 if my_choice > 2 or my_choice < 0:
     print("You are type an invalid number. You lose!")
+else:
+    # pintar la elección del user:
+    print(hand_drawing[my_choice])
+    #generar la elección aleatoria de computer:
+    computer_choice = random.randint(0, 2)
+    # pintar la elección de computer:
+    print(f"Computer Choose: {hand_drawing[computer_choice]}")
+
+#Lógica del juego:
+
 # rock against ...
-elif my_choice == 0 and computer_choice == 0:
+if my_choice == 0 and computer_choice == 0:
     print("This is a tie")
 elif my_choice == 0 and computer_choice == 1:
     print("You loose")
