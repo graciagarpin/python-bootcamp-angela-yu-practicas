2b7bd4a535c8a535d5fa04ee21c0f740bfedc9a9
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-06-18 23:00:03 +0200

    TODO 2: Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.

--- hangman/hangman_project.py
+++ hangman/hangman_project.py
@@ -1,3 +1,5 @@
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
@@ -8,9 +10,9 @@ word_list = ["aardvark", "baboon", "camel"]
 #  is, "Wrong" if it's not.
 
 # TODO-1 :
-
-import random
-
 chosen_word = random.choice(word_list)
-
 print(chosen_word)
+
+# TODO-2:
+guess = str(input("Guess a letter: ")).lower()
+print(guess)
