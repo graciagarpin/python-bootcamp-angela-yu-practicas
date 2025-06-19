87c14dc23a5937a50c89906dfe886d2d6bc3b480
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-06-19 15:27:47 +0200

    delete TODOs commented

--- hangman/hangman_project.py
+++ hangman/hangman_project.py
@@ -2,22 +2,12 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-
-# TODO-1:
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-2:
 guess = input("Guess a letter: ").lower()
 print(guess)
 
-# TODO-3:
 for letter in chosen_word:
     if letter == guess:
         print("True")
