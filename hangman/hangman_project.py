26a16e7953dd0cc77e39fd5b3637f5f8b5c2cc11
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-06-26 12:51:41 +0200

    If the letter at that position matches guess then reveal that letter in the display at that position.

--- hangman/hangman_project.py
+++ hangman/hangman_project.py
@@ -14,10 +14,17 @@ for letter in chosen_word:
 print(placeholder)
 
 guess = input("Guess a letter: ").lower()
-print(guess)
+#Create an empty string called "display".
+#Loop through each letter in the chosen_word
+#If the letter at that position matches guess then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be _ p p _ _.
+#Print display and you should see the guessed letter in the correct position.
+#But every letter that is not a match is represented with a "_".
+
+display = ""
 
 for letter in chosen_word:
     if letter == guess:
-        print("True")
+        print(letter)
     else:
-        print("Wrong")
+        print("_")
