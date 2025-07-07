6e334721ee7e024a93cbaa911e511951a26dee04
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-07 15:58:40 +0200

    add the feedback to he user if they win or lose

--- hangman/hangman_project.py
+++ hangman/hangman_project.py
@@ -47,12 +47,10 @@ while not game_over:
         print(f'You guessed {guess}, that\'s not in the word. You lose a life')
         if lives == 0:
             game_over = True
-            print("You lose.")
+            print(f"***********************YOU LOSE**********************")
 
     if "_" not in display:
         game_over = True
-        print("You win.")
+        print("****************************YOU WIN****************************")
 
     print(stages[lives])
-else:
-    print("You win.")
