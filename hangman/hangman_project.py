5f2be5c6bb6fa2888f623691ee5e281339caf427
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-07 14:51:54 +0200

    remove all comments

--- hangman/hangman_project.py
+++ hangman/hangman_project.py
@@ -1,14 +1,11 @@
 import random
 
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 import hangman_art
 import hangman_words
 
 lives = 6
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-print(hangman_art.logo)
 
-#from hangman_words import logo
+print(hangman_art.logo)
 
 chosen_word = random.choice(hangman_words.word_list)
 
@@ -40,10 +37,6 @@ while not game_over:
 
     print(display)
 
-# TODO-2:
-# If guess is not a letter in the chosen_word, Then reduce lives by 1.
-# If lives goes down to 0 then the game should end, and it should print "You lose."
-
     if guess not in chosen_word:
         lives -= 1
         print(f'You guessed {guess}, that\'s not in the word. You lose a life')
@@ -55,11 +48,7 @@ while not game_over:
         game_over = True
         print("You win.")
 
-    # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
     print(hangman_art.stages[lives])
     print(f"****************************{lives}/6 LIVES LEFT****************************")
 else:
     print("You win.")
-
-#Phrase to display on screen when the letter has not been guessed:
-    #print(f'You guessed {letter}, that\'s not in the word. You lose a life')
