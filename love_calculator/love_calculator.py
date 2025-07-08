dee82508df91b6aa26c1e2fef607f56f0f43d3d2
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-08 14:24:32 +0200

    add an improved version of the project by me

--- love_calculator/love_calculator.py
+++ love_calculator/love_calculator.py
@@ -1,19 +1,18 @@
-name1= input("Please, write your name: ")
-name2= input("Please, write the name of the person you love: ")
-
 def calculate_love_score(name1, name2):
-    word1 = "TRUE"
-    word2 = "LOVE"
+    love_phrase = "TRUE LOVE"
+    # print(love_phrase.split()[0])
+    # print(love_phrase.split()[1])
+
     points_word1 = 0
     points_word2 = 0
     for name_letter in name1.lower() + name2.lower():
         # print(name_letter)
-        for word_letter in word1.lower():
+        for word_letter in love_phrase.split()[0].lower():
             # print(word_letter)
             if name_letter == word_letter:
                 points_word1 += 1
                 # print("si")
-        for word_letter in word2.lower():
+        for word_letter in love_phrase.split()[1].lower():
             # print(word_letter)
             if name_letter == word_letter:
                 points_word2 += 1
@@ -21,7 +20,6 @@ def calculate_love_score(name1, name2):
     # print(points_word1)
     # print(points_word2)
     love_score = str(points_word1) + str(points_word2)
-    return print(f"The love score of the couple is: {love_score}/100")
+    return print(love_score)
 
-
-calculate_love_score(name1, name2)
+calculate_love_score("Angela Yu", "Keanu Reeves")
