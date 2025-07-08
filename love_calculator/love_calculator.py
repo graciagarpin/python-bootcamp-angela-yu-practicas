84fbf538c6b53c7970565ee2c3a3eab068d178e2
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-08 14:31:40 +0200

    add inputs to ask the user for the names, add the feedback with the total Score and remove comments

--- love_calculator/love_calculator.py
+++ love_calculator/love_calculator.py
@@ -1,25 +1,19 @@
+name1 = input("Please, write your name: ")
+name2 = input("Please, write the name of the person you love: ")
+
 def calculate_love_score(name1, name2):
     love_phrase = "TRUE LOVE"
-    # print(love_phrase.split()[0])
-    # print(love_phrase.split()[1])
 
     points_word1 = 0
     points_word2 = 0
     for name_letter in name1.lower() + name2.lower():
-        # print(name_letter)
         for word_letter in love_phrase.split()[0].lower():
-            # print(word_letter)
             if name_letter == word_letter:
                 points_word1 += 1
-                # print("si")
         for word_letter in love_phrase.split()[1].lower():
-            # print(word_letter)
             if name_letter == word_letter:
                 points_word2 += 1
-                # print("si")
-    # print(points_word1)
-    # print(points_word2)
     love_score = str(points_word1) + str(points_word2)
-    return print(love_score)
+    return print(f"The Love Score of the couple is: {love_score}/100")
 
-calculate_love_score("Angela Yu", "Keanu Reeves")
+calculate_love_score(name1, name2)
