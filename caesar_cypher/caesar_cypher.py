7af1efcc3021ec76d27e8aa1f69b659f9e31293a
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-10 14:35:25 +0200

    Inside the 'encrypt() shift each letter of the 'original_text' forwards in the alphabet by the shift amount and print the encrypted text

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -8,15 +8,29 @@ shift = int(input("Type the shift number:\n"))
 # TODO1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
 
 def encrypt(original_text, shift_amount):
-    print(original_text)
-    print(shift_amount)
-
-encrypt(text, shift)
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
+    encrypted_text = ""
+    for text_letter in original_text:
+        print(text_letter)
+        for alphabet_letter in alphabet: # cogemos la letra "h" de la palabra "hi" y el shift = 1
+            if text_letter == alphabet_letter:
+                index_letter = alphabet.index(text_letter)
+                # index = 7
+                print(index_letter)
+                index_encoded_letter = index_letter + shift_amount
+                print(index_encoded_letter)
+                print(alphabet[index_encoded_letter])
+                encrypted_text += alphabet[index_encoded_letter]
 
+    else:
+        print("salida")
+
+    return print(encrypted_text)
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
+
+encrypt(text, shift)
