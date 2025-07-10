946a3e4bc60cfec13e7125df8e943c9fc8d31226
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-10 14:53:13 +0200

    improve encrypt function logic removing for and if: alphabet.index search for que index on the alphabet list directly

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -7,25 +7,19 @@ shift = int(input("Type the shift number:\n"))
 # PART 1: ENCODE
 # TODO1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
 
-def encrypt(original_text, shift_amount):
-
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
-    encrypted_text = ""
-    for text_letter in original_text:
-        print(text_letter)
-        for alphabet_letter in alphabet: # cogemos la letra "h" de la palabra "hi" y el shift = 1
-            if text_letter == alphabet_letter:
-                index_letter = alphabet.index(text_letter)
-                # index = 7
-                print(index_letter)
-                index_encoded_letter = index_letter + shift_amount
-                print(index_encoded_letter)
-                print(alphabet[index_encoded_letter])
-                encrypted_text += alphabet[index_encoded_letter]
 
-    else:
-        print("salida")
+# original_text = hi , shift_amount = 1
+def encrypt(original_text, shift_amount):
+    encrypted_text = ""
+    for text_letter in original_text: # h
+
+        index_letter = alphabet.index(text_letter) # index = 7
+
+        index_encoded_letter = index_letter + shift_amount # index_encoded = 8
+
+        encrypted_text += alphabet[index_encoded_letter]
 
     return print(encrypted_text)
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
