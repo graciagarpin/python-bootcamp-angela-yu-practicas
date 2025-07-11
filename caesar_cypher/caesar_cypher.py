d8a8fd5f5324a7046be300dceb38498884716532
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-11 23:31:45 +0200

    keep and print non-alphabetic characters as entered by the user

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -2,6 +2,8 @@ from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+#
+
 print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -12,9 +14,14 @@ def caesar(encode_or_decode, original_text, shift_amount):
     if encode_or_decode == 'decode':
         shift_amount = shift_amount * -1
     for text_letter in original_text:
-        shifted_position = alphabet.index(text_letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if text_letter not in alphabet:
+            output_text += text_letter
+        else:
+            shifted_position = alphabet.index(text_letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
     print(f"Here is the encoded result: {output_text}")
+
+# TODO-2: restart the cypher program
 
 caesar(direction, text, shift)
