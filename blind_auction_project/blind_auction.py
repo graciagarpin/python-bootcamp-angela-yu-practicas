a7603bcf2b8a5d038e6078566619d89a7f14a1a7
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-19 15:10:23 +0200

    ask user for a name and ask user for a bid price

--- blind_auction_project/blind_auction.py
+++ blind_auction_project/blind_auction.py
@@ -4,7 +4,14 @@ print(logo)
 
 # TODO 1: ask user for a name and ask user for a bid price
 
+user_name = input("What is your name?:")
+user_price = int(input("What is your bid?: $ "))
+
+bids = {} #empty dictionary
+
 # TODO 2: Save data into a Dictionary: key - value
+
+bid = {user_name: user_price}
 
 # TODO 3: Whether if new bids need to be added
 
