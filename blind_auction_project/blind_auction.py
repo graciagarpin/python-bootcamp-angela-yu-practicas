cdc37d2af2da1963a68778ea4bb344382b048b00
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-19 17:08:15 +0200

    Add a game start or end control statement and accept new auction bids

--- blind_auction_project/blind_auction.py
+++ blind_auction_project/blind_auction.py
@@ -2,17 +2,26 @@ from art import logo
 
 print(logo)
 
-# TODO 1: ask user for a name and ask user for a bid price
-
-user_name = input("What is your name?:")
-user_price = int(input("What is your bid?: $ "))
-
-bids = {} #empty dictionary
-
-# TODO 2: Save data into dictionary: key - value
-
-bids[user_name] = user_price
+# TODO 4: Compare bids in dictionary
+def compare_bids():
+    print("comparing bids")
 
 # TODO 3: Whether if new bids need to be added
 
-# TODO 4: Compare bids in dictionary
+bids = {} #empty dictionary
+
+turn_off_game = False
+while not turn_off_game:
+    # TODO 1: ask user for a name and ask user for a bid price
+    user_name = input("What is your name?:")
+    user_price = int(input("What is your bid?: $ "))
+
+    # TODO 2: Save data into dictionary: key - value
+    bids[user_name] = user_price
+    print(bids) # we have only one bidder stored in the dictionary.
+
+    another_bidder = input("Are there any other bidders? Type 'yes' or 'no': ")
+    if another_bidder == 'no':
+        print(bids)
+        compare_bids()
+        turn_off_game = True
