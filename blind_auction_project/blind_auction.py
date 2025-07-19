725e8611a56f4e57e8de33c8a72231d058986142
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-19 15:09:33 +0200

    define initial TODO list

---
+++ blind_auction_project/blind_auction.py
@@ -0,0 +1,11 @@
+from art import logo
+
+print(logo)
+
+# TODO 1: ask user for a name and ask user for a bid price
+
+# TODO 2: Save data into a Dictionary: key - value
+
+# TODO 3: Whether if new bids need to be added
+
+# TODO 4: Compare bids in dictionary
