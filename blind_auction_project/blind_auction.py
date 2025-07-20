c5c1e37aeed4dffae8c112fd804c916e55d10880
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-20 19:50:53 +0200

    quick fix of variable namming and cleen the screen for the next bidder

--- blind_auction_project/blind_auction.py
+++ blind_auction_project/blind_auction.py
@@ -3,19 +3,19 @@ from art import logo
 print(logo)
 
 # TODO 4: Compare bids in dictionary
-def compare_bids(total_bids):
+def find_highest_bidder(total_bids):
     highest_bid_amount = 0
     winner = ""
 
-    for name, bid in total_bids.items():
+    for bidder, bid in total_bids.items():
         if bid > highest_bid_amount:
             highest_bid_amount = bid
-            winner = name
+            winner = bidder
     return print(f"The winner is {winner} with a bid of ${highest_bid_amount}")
 
 # TODO 3: Whether if new bids need to be added
 
-bids = {} #empty dictionary
+bids = {} # It is necessary to start the bid with an empty dictionary.
 
 turn_off_game = False
 while not turn_off_game:
@@ -24,11 +24,15 @@ while not turn_off_game:
     user_price = int(input("What is your bid?: $ "))
 
     # TODO 2: Save data into dictionary: key - value
-    bids[user_name] = user_price
-    print(bids) # we have only one bidder stored in the dictionary.
+    bids[user_name] = user_price # we have only one bidder stored in the dictionary.
 
     should_continue = input("Are there any other bidders? Type 'yes' or 'no': \n".lower())
     if should_continue == 'no':
-        print(bids)
-        compare_bids(bids)
+        find_highest_bidder(bids)
         turn_off_game = True
+    elif should_continue == 'yes':
+        print("\n" * 20)
+        '''
+    elif should_continue != 'no' or should_continue != 'yes':
+        print("Please, type 'yes' or 'no'")
+        '''
