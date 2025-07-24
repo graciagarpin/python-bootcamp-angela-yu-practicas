0275dd9f7328eecae17e68664882dd51f5feb50e
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-07-24 17:05:15 +0200

    If the user asks yes to continue program loops to use the previous result as the first number and then repeats the calculation process.

--- calculator project/calculator.py
+++ calculator project/calculator.py
@@ -1,9 +1,6 @@
-# TODO1: Print calc logo
 import art
 
 print(art.logo)
-
-#TODO 2: calculate operation and print the result (float) ej: 2.0 + 4.0 = 6.0
 
 def add(n1, n2):
     return n1 + n2
@@ -24,12 +21,11 @@ operations = {
     "/": divide,
 }
 
-# Ask user for a first number, operation_symbol and next number
-
 should_continue = True
 first_num = float(input("What's the first number?: "))
 
 while should_continue:
+    current_result = first_num
     for symbol in operations:
         print(symbol)
     operation_symbol = input("Pick an operation: ")
@@ -37,10 +33,11 @@ while should_continue:
     answer = operations[operation_symbol](first_num, next_num)
     print(f"{first_num} {operation_symbol} {next_num} = {answer}")
 
-# TODO 3: Ask user if wants to continue calculating with result or restart the calculation.
     restart = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
     print(restart)
     if restart == 'y':
         should_continue = True
+        first_num = answer
     else:
         should_continue = False
+        print("\n" * 20)
