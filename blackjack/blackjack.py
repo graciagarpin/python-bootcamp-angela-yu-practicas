912295eddaec66a7109abd03b33cc98ec67191f0
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-08-05 15:51:17 +0200

    pick up  2 cards to each player ramdomly and add to the list

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -7,13 +7,14 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # if asks 'y':
 # TODO 1: create 2 empty lists to hold user card and computer card
 
-#by the moment mocked values:
-user_cards = [2, 4]
-computer_cards = [6, 7]
+user_cards = []
+computer_cards = []
 
 # TODO 2: pick up  2 cards to each player ramdomly and add to the list. Pick up from cards's list.
 
-random.choice(cards)
+for _ in range(2):
+    value = random.sample(cards, 2)
+    print(value)
 
 # TODO 3: Calculate and add up players's scores
 
@@ -29,6 +30,6 @@ user_score = calculate_score(user_cards)
 computer_score = calculate_score(computer_cards)
 
 # print logo
-print(f"Your cards: {user_cards}, current score: {user_score}\nComputer's first card: {computer_cards[0]}")
+# print(f"Your cards: {user_cards}, current score: {user_score}\nComputer's first card: {computer_cards[0]}")
 #  print("Type 'y' to get another card, type 'n' to pass: ")
 
