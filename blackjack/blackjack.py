03b9276d198c38768264740f9009c2ee73f42d3f
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-08-05 15:43:39 +0200

    print the user hand and user current score, and print the Computer's first card

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -4,9 +4,10 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 #todo 0: Start game:
 # print("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-
+# if asks 'y':
 # TODO 1: create 2 empty lists to hold user card and computer card
 
+#by the moment mocked values:
 user_cards = [2, 4]
 computer_cards = [6, 7]
 
@@ -14,7 +15,7 @@ computer_cards = [6, 7]
 
 random.choice(cards)
 
-# TODO 3: Calculate and add up gamers's scores
+# TODO 3: Calculate and add up players's scores
 
 # calculate score adding the values of the cards:
 
@@ -26,12 +27,8 @@ def calculate_score(list_of_values):
 
 user_score = calculate_score(user_cards)
 computer_score = calculate_score(computer_cards)
-print(user_score)
-print(computer_score)
 
-# if asks 'y':
 # print logo
-# print("Your cards: {[a,b]}, current score: {c}
-#       \nComputer's first card: {d})
+print(f"Your cards: {user_cards}, current score: {user_score}\nComputer's first card: {computer_cards[0]}")
 #  print("Type 'y' to get another card, type 'n' to pass: ")
 
