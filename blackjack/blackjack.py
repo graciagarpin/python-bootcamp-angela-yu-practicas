0ccb198a451648193391dfee88c876fe3ffbc450
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-08-25 15:59:01 +0200

    check for a blackjackon the first hand

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -18,16 +18,38 @@ def deal_card():
 # TODO 3: Calculate players's scores: ✓
 
 def calculate_score(list_of_values):
+
     return sum(list_of_values)
+
 
 # TODO 2.2: pick up  2 cards to each player ramdomly and add to the list: ✓
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-#add up scores:
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+# TODO 6: check for a blackjack (a hand with only 2 cards: ace + 10)
+# and return False instead of the actual score.
+def is_blackjack_on_first_hand(hand_values):
+    blackjack_values = [11, 10]
+
+    for value in blackjack_values:
+        if value not in hand_values:
+            return False
+    return True
+
+#################
+print(is_blackjack_on_first_hand(user_cards))
+print(is_blackjack_on_first_hand(computer_cards))
+
+### end of the game or continue??
+if is_blackjack_on_first_hand(user_cards):
+    print("You win")
+elif is_blackjack_on_first_hand(computer_cards):
+    print("You lose")
+else:
+    #add up scores:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
 # print logo
 print(f"Your cards: {user_cards}, current score: {user_score}\nComputer's first card: {computer_cards[0]}")
@@ -41,4 +63,4 @@ elif computer_score == 21:
 else user_score =! 21:
 '''
 # TODO 5: Ask for another card
-should_continue = input(print("Type 'y' to get another card, type 'n' to pass: "))
+# should_continue = input(print("Type 'y' to get another card, type 'n' to pass: "))
