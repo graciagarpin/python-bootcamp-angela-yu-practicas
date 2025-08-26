4bde82a26c41f3d64e6a4f32143c985e7eb70f44
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-08-26 13:02:06 +0200

    Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -17,39 +17,22 @@ def deal_card():
 
 # TODO 3: Calculate players's scores: ✓
 
-def calculate_score(list_of_values):
-
-    return sum(list_of_values)
-
+def calculate_score(cards):
+    # TODO 6: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10)
+    #  and return 0 instead of the actual score. 0 will represent a blackjack.
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    return sum(cards)
 
 # TODO 2.2: pick up  2 cards to each player ramdomly and add to the list: ✓
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-# TODO 6: check for a blackjack (a hand with only 2 cards: ace + 10)
-# and return False instead of the actual score.
-def is_blackjack_on_first_hand(hand_values):
-    blackjack_values = [11, 10]
 
-    for value in blackjack_values:
-        if value not in hand_values:
-            return False
-    return True
-
-#################
-print(is_blackjack_on_first_hand(user_cards))
-print(is_blackjack_on_first_hand(computer_cards))
-
-### end of the game or continue??
-if is_blackjack_on_first_hand(user_cards):
-    print("You win")
-elif is_blackjack_on_first_hand(computer_cards):
-    print("You lose")
-else:
-    #add up scores:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
+#add up scores:
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
 
 # print logo
 print(f"Your cards: {user_cards}, current score: {user_score}\nComputer's first card: {computer_cards[0]}")
