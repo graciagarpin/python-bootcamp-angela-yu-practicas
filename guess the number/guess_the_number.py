a0ea769e872732aa7b18d24cee3bb75089c019e8
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-09-10 17:39:22 +0200

    todo list and TODO 1 and 2 done

---
+++ guess the number/guess_the_number.py
@@ -0,0 +1,13 @@
+# TODO LIST
+# TODO 1: print: welcome to the Number Guessing Game! I'm thinking of a number between 1 and 100
+# TODO : generate a random number between 1 and 100 and store in the variable "number". This will be the number to guess.
+# TODO : print input: Choose a difficulty. Type 'easy' or 'hard':
+# TODO  - LEVEL OF DIFFICULTY -: easy: You have 10 attemps remaining to guess the number. hard: You have 5 attemps remaining to guess the number.
+# TODO : print input: Make a guess:
+# TODO : Compare the guess with the number.
+    # TODO .1: if the guess is higher than the number, print: Too high. n/Guess again. n/You have {lives} attemps remaining to guess the number.
+    # TODO .2: if the guess is lower than the number, print: Too low. n/Guess again. n/You have {lives} attemps remaining to guess the number.
+# TODO : after each failed attempt, you lose a life.
+# TODO : You lose when you run out the lives. You win when you guess the number.
+# TODO : print LOGO
+# while you remain lives, you can play.
