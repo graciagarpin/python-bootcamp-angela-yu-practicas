30443a6e37693c8b4759e5b32eb545cd9c75464e
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-09-11 15:49:34 +0200

    Compare the guess with the number and after each failed attempt, rest a life

--- guess the number/guess_the_number.py
+++ guess the number/guess_the_number.py
@@ -3,11 +3,12 @@
 # ✔ TODO 2: generate a random number between 1 and 100 and store in the variable "number". This will be the number to guess.
 # ✔ TODO 3: Choose a difficulty. Type 'easy' or 'hard':
     # ✔ TODO - select the LEVEL OF DIFFICULTY -: easy: You have 10 attemps remaining to guess the number. hard: You have 5 attemps remaining to guess the number.
-# TODO 4: print input: Make a guess:
-# TODO : Compare the guess with the number.
-    # TODO .1: if the guess is higher than the number, print: Too high. n/Guess again. n/You have {lives} attemps remaining to guess the number.
-    # TODO .2: if the guess is lower than the number, print: Too low. n/Guess again. n/You have {lives} attemps remaining to guess the number.
-# TODO : after each failed attempt, you lose a life.
+# TODO 4: Compare the guess with the number.
+    # ✔ TODO 4.1: print input: Make a guess:
+    # ✔  TODO 4.2: if the guess is higher than the number, print: Too high. n/Guess again. n/You have {lives} attemps remaining to guess the number.
+    # ✔  TODO 4.3: if the guess is lower than the number, print: Too low. n/Guess again. n/You have {lives} attemps remaining to guess the number.
+    # ✔ TODO : after each failed attempt, you lose a life.
+# TODO 5 : While the number is not guessed, a new attempt will be requested, until all lives are lost.
 # TODO : You lose when you run out guesses. You win when you guess the number.
     # You've run out of guesses. Refresh the page to run again.
     # You got it! The answer was {number}
@@ -15,7 +16,6 @@
 # while you remain lives, you can play.
 
 import random
-from symbol import return_stmt
 
 print("Welcome to the Number Guessing Game! I'm thinking of a number between 1 and 100")
 
@@ -23,11 +23,8 @@ number = random.randint(1, 100)
 
 print(number)
 
-lives = 0
-
 def select_difficulty_level(lives):
-    level_of_difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
-
+    level_of_difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
     if level_of_difficulty == "easy":
         print("You have 10 attemps remaining to guess the number.")
@@ -38,6 +35,26 @@ def select_difficulty_level(lives):
     else:
         return "type again" #cambiar más adelante
 
+
+def compare_the_guess(guess, number, lives):
+    if guess > number:
+        lives -= 1
+        print(f"Too high.\nGuess again. \nYou have {lives} attemps remaining to guess the number.")
+        return lives
+    elif guess < number:
+        lives -= 1
+        print(f"Too low.\nGuess again. \nYou have {lives} attemps remaining to guess the number.")
+        return lives
+    else:
+        print(f"You got it! The answer was {number}")
+        return lives
+
+lives = 0
+
 lives = select_difficulty_level(lives)
 
-guess = input("Make a guess: ")
+guess = int(input("Make a guess: "))
+
+lives = compare_the_guess(guess, number, lives)
+
+print(lives)
