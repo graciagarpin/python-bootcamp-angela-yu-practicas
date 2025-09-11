05768c7cc4010b1ef8480b8db9e46261f0363d75
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-09-11 17:26:38 +0200

    While we have more than 0 lives, a new attempt will be requested, until all lives are lost. missing todo: close the game when the user guess the number

--- guess the number/guess_the_number.py
+++ guess the number/guess_the_number.py
@@ -5,10 +5,12 @@
     # ✔ TODO - select the LEVEL OF DIFFICULTY -: easy: You have 10 attemps remaining to guess the number. hard: You have 5 attemps remaining to guess the number.
 # TODO 4: Compare the guess with the number.
     # ✔ TODO 4.1: print input: Make a guess:
-    # ✔  TODO 4.2: if the guess is higher than the number, print: Too high. n/Guess again. n/You have {lives} attemps remaining to guess the number.
-    # ✔  TODO 4.3: if the guess is lower than the number, print: Too low. n/Guess again. n/You have {lives} attemps remaining to guess the number.
-    # ✔ TODO : after each failed attempt, you lose a life.
-# TODO 5 : While the number is not guessed, a new attempt will be requested, until all lives are lost.
+    # ✔  TODO 4.2: if the guess is higher than the number, print: Too high.
+    # ✔  TODO 4.3: if the guess is lower than the number, print: Too low.
+    # ✔  TODO 4.4: if the guess match with the number. print: You got it!
+    # ✔ TODO : after each failed attempt, you lose a life
+# TODO 5 : While the number is not guessed, a new attempt will be requested, until all lives are lost:
+    #  and print: Guess again. n/You have {lives} attemps remaining to guess the number.
 # TODO : You lose when you run out guesses. You win when you guess the number.
     # You've run out of guesses. Refresh the page to run again.
     # You got it! The answer was {number}
@@ -39,22 +41,27 @@ def select_difficulty_level(lives):
 def compare_the_guess(guess, number, lives):
     if guess > number:
         lives -= 1
-        print(f"Too high.\nGuess again. \nYou have {lives} attemps remaining to guess the number.")
+        print("Too high.")
         return lives
     elif guess < number:
         lives -= 1
-        print(f"Too low.\nGuess again. \nYou have {lives} attemps remaining to guess the number.")
+        print("Too low.")
         return lives
     else:
-        print(f"You got it! The answer was {number}")
-        return lives
+        print("You got it!")
 
 lives = 0
 
 lives = select_difficulty_level(lives)
 
-guess = int(input("Make a guess: "))
+# TODO 5 : While we have more than 0 lives, a new attempt will be requested, until all lives are lost:
+    #  and print: Guess again. n/You have {lives} attemps remaining to guess the number.
 
-lives = compare_the_guess(guess, number, lives)
-
-print(lives)
+while lives > 0:
+    guess = int(input("Make a guess: "))
+    lives = compare_the_guess(guess, number, lives)
+    print(f"Guess again. \nYou have {lives} attemps remaining to guess the number.")
+else:
+    print("You've run out of guesses. Refresh the page to run again.")
+    # when the user win, to show the following message:
+    print(f"The answer was {number}")
