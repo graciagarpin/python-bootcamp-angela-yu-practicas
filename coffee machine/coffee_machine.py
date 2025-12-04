fdb1baeffb885ad379e7f8321e2a08f8cc3f30ad
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-12-04 15:47:21 +0100

    check if there are enough resources to make the coffee

--- coffee machine/coffee_machine.py
+++ coffee machine/coffee_machine.py
@@ -36,9 +36,14 @@ resources = {
 # TODO 3 Resources resume: Print report when the user enters “report” to the prompt.
 #  this report shows the current resource values.
 # TODO 4 : check if there are enough resources to make that drink.
-#def is_sufficient_resource(order_ingredient): # You have selected a latte
-    #for ingredient in drink["ingredients"]: # {'water': 200, 'milk': 150, 'coffee': 24}
-        #if order_ingredient >= ingredient.values:
+# TODO: if there are not enough or zero resources, it do not continue to make the drink and print: “Sorry there is not enough {ingredient}.”
+
+def is_sufficient_resource(order_ingredient): # You have selected a latte
+    for element in order_ingredient: # {'water': 200, 'milk': 150, 'coffee': 24}
+        if order_ingredient[element] >= resources[element]:
+            print(f"Sorry, there is not enough{element}.")
+            return False
+    return True
 
 is_on = True
 
@@ -55,11 +60,8 @@ while is_on:
     elif choice == "latte" or choice == "espresso" or choice == "capuccino":
         print(f"You have selected a {choice}")
         drink = MENU[choice]
-        #is_sufficient_resource(drink["ingredients"])
         print(drink["ingredients"])
+        print(is_sufficient_resource(drink["ingredients"]))
     else:
        print(f"Please, type your choice again")
 
-# is_sufficient_resources()
-
-# TODO: if there are not enough or zero resources, it do not continue to make the drink and print: “Sorry there is not enough water.”
