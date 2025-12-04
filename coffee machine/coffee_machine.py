8ea7e4b72a35a72d28ec85955736b395705ce9b3
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-12-04 19:49:34 +0100

    Calculate the monetary value of the coins inserted. pending correction

--- coffee machine/coffee_machine.py
+++ coffee machine/coffee_machine.py
@@ -27,8 +27,15 @@ MENU = {
 profit = 0
 resources = {
     "water": 300,
-    "milk": 200,
+    "milk": 100,
     "coffee": 100
+}
+
+COINS = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01
 }
 
 # TODO 1: Prompt user what drink they want.
@@ -36,14 +43,22 @@ resources = {
 # TODO 3 Resources resume: Print report when the user enters “report” to the prompt.
 #  this report shows the current resource values.
 # TODO 4 : check if there are enough resources to make that drink.
-# TODO: if there are not enough or zero resources, it do not continue to make the drink and print: “Sorry there is not enough {ingredient}.”
+# TODO 5 : if there are not enough or zero resources, it do not continue to make the drink and print: “Sorry there is not enough {ingredient}.”
 
-def is_sufficient_resource(order_ingredient): # You have selected a latte
-    for element in order_ingredient: # {'water': 200, 'milk': 150, 'coffee': 24}
+def is_sufficient_resource(order_ingredient): #(drink["ingredients"]) latte
+    for element in order_ingredient:  # {'water': 200, 'milk': 150, 'coffee': 24}
         if order_ingredient[element] >= resources[element]:
-            print(f"Sorry, there is not enough{element}.")
+            print(f"Sorry, there is not enough {element}.")
             return False
     return True
+
+def calculate_amount(list_coins, coins_values):
+    total = 0
+    print(coins_values)
+    for coin in list_coins:
+        amount_of_coin = coin * coins_values[coin]
+        total += amount_of_coin
+    print(total)
 
 is_on = True
 
@@ -60,8 +75,20 @@ while is_on:
     elif choice == "latte" or choice == "espresso" or choice == "capuccino":
         print(f"You have selected a {choice}")
         drink = MENU[choice]
-        print(drink["ingredients"])
-        print(is_sufficient_resource(drink["ingredients"]))
+        is_enough = is_sufficient_resource(drink["ingredients"])
+        if is_enough:
+            print("Please, insert coins.")
+            quarters = input("How many quarters?: ")
+            dimes = input("How many dimes?: ")
+            nickles = input("How many nickles?: ")
+            pennies = input("How many pennies?: ")
+            calculate_amount([quarters, dimes, nickles, pennies], COINS)
     else:
        print(f"Please, type your choice again")
 
+# TODO 6: If there are sufficient resources to make the drink selected,
+#  then the program should prompt the user to insert coins.
+
+# TODO 7: Calculate the monetary value of the coins inserted.
+# E.g. 1 quarter, 2 dimes, 1 nickel, 2
+# pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
