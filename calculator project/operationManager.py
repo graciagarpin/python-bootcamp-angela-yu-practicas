87c08fc996136683a8b3f4e4df225d14b5fbcd42
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-12-15 15:23:37 +0100

    add 2 files to separate poo

---
+++ calculator project/operationManager.py
@@ -0,0 +1,14 @@
+
+#TODO:
+
+# definir la clase OperationManager
+
+class OperationManager:
+    def __init__(self):
+        self.operations = {...}
+
+# contener métodos matemáticos
+
+# contener el diccionario de símbolos → métodos
+
+#MÉTODOS
