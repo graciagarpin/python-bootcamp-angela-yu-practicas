d8d25f073dd8d1211269eac70e83d25bd6dff1f8
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-12-19 13:50:44 +0100

    define operations methods

--- calculator project/operations.py
+++ calculator project/operations.py
@@ -2,8 +2,9 @@
 #TODO:
 
 # definir la clase OperationManager:
-# debe contener métodos matemáticos
 # debe contener el diccionario de símbolos → métodos
+# debe contener métodos matemáticos
+# no calcula, si no que provee de operaciones a calculator
 
 class OperationManager:
     def __init__(self):
@@ -14,6 +15,15 @@ class OperationManager:
             "/": self.divide,
         }
 
+    # MÉTODOS
+    def add(self, n1, n2):
+            return n1 + n2
 
+    def subtract(self, n1, n2):
+            return n1 - n2
 
-#MÉTODOS
+    def multiply(self, n1, n2):
+            return n1 * n2
+
+    def divide(self, n1, n2):
+            return n1 / n2
