ed13a75c870fc318723a9dedebf98753275206df
LAPTOP-BQNGAEFP\graci <graciagarpin>
2025-12-23 19:35:22 +0100

    initial commmit

---
+++ higher lower game/higher_lower.py
@@ -0,0 +1,28 @@
+import art
+
+# TODO 1: define the class in charge of orchestrating the game
+
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print(art.logo)
+        is_on = True
+        while is_on:
+            pass
+
+
+# prints, inputs, etc... here, in the main file:
+# "Compare A: {"name"}, {"description"}, from {"country"}"
+# show ASCII art logo "vs."
+# "Against B: {"name"}, {"description"}, from {"country"}"
+# "Who has more followers? Type 'A' or 'B': "
+
+# check if the user has selected the right option:
+# compare followers option A and option B. it is the selected option the higher?
+# if it is,
+
+# You're right! Current score: {score}
+
+# Sorry, that's wrong. Final score: {score}
